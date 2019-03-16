7026355b273f4fca55e98742aabbeab686bdabf4
ananas <906939864@qq.com>
2019-03-16 19:11:42 +0800

    Delete weapp-videos.js

--- react-videos/weapp-videos/weapp-videos.js
+++
@@ -1,9 +0,0 @@
-Component({
-  data: { videoActive: false, currentActive: null },
-  methods: {
-    handleVideoActive(evt) {
-      const { videonum } = evt.currentTarget.dataset;
-      this.setData({ videoActive: true, currentActive: videonum });
-    },
-  },
-});
